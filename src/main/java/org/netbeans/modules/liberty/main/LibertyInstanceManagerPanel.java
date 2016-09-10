b390a61e61187ac9710c93e738b8a751ada2c434
Stephan Knitelius <stephan@knitelius.com>
2016-09-10 14:50:14 +0200

    Added location chooser pop-up, to select the Liberty installation location.

--- src/main/java/org/netbeans/modules/liberty/main/LibertyInstanceManagerPanel.java
+++ src/main/java/org/netbeans/modules/liberty/main/LibertyInstanceManagerPanel.java
@@ -4,7 +4,8 @@ package org.netbeans.modules.liberty.main;
  * Panel in Server Manager under Tools menu
  * and in Project Properties of instance in Services window
  * for registering instances, seeing properties, etc.
- * @author gwieleng
+ * 
+ * @author gwieleng & sknitelius
  */
 public class LibertyInstanceManagerPanel extends javax.swing.JPanel {
 
@@ -24,15 +25,33 @@ public class LibertyInstanceManagerPanel extends javax.swing.JPanel {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jLabel1 = new javax.swing.JLabel();
-        jTextField1 = new javax.swing.JTextField();
-        jButton1 = new javax.swing.JButton();
+        jFileChooser1 = new javax.swing.JFileChooser();
+        jFrame1 = new javax.swing.JFrame();
+        locationLabel = new javax.swing.JLabel();
+        installationLocation = new javax.swing.JTextField();
+        browseButton = new javax.swing.JButton();
 
-        org.openide.awt.Mnemonics.setLocalizedText(jLabel1, org.openide.util.NbBundle.getMessage(LibertyInstanceManagerPanel.class, "LibertyInstanceManagerPanel.jLabel1.text")); // NOI18N
+        javax.swing.GroupLayout jFrame1Layout = new javax.swing.GroupLayout(jFrame1.getContentPane());
+        jFrame1.getContentPane().setLayout(jFrame1Layout);
+        jFrame1Layout.setHorizontalGroup(
+            jFrame1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 400, Short.MAX_VALUE)
+        );
+        jFrame1Layout.setVerticalGroup(
+            jFrame1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 300, Short.MAX_VALUE)
+        );
 
-        jTextField1.setText(org.openide.util.NbBundle.getMessage(LibertyInstanceManagerPanel.class, "LibertyInstanceManagerPanel.jTextField1.text")); // NOI18N
+        org.openide.awt.Mnemonics.setLocalizedText(locationLabel, org.openide.util.NbBundle.getMessage(LibertyInstanceManagerPanel.class, "LibertyInstanceManagerPanel.locationLabel.text")); // NOI18N
 
-        org.openide.awt.Mnemonics.setLocalizedText(jButton1, org.openide.util.NbBundle.getMessage(LibertyInstanceManagerPanel.class, "LibertyInstanceManagerPanel.jButton1.text")); // NOI18N
+        installationLocation.setText(org.openide.util.NbBundle.getMessage(LibertyInstanceManagerPanel.class, "LibertyInstanceManagerPanel.installationLocation.text")); // NOI18N
+
+        org.openide.awt.Mnemonics.setLocalizedText(browseButton, org.openide.util.NbBundle.getMessage(LibertyInstanceManagerPanel.class, "LibertyInstanceManagerPanel.browseButton.text")); // NOI18N
+        browseButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                browseButtonActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
@@ -42,11 +61,11 @@ public class LibertyInstanceManagerPanel extends javax.swing.JPanel {
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jTextField1, javax.swing.GroupLayout.DEFAULT_SIZE, 455, Short.MAX_VALUE)
+                        .addComponent(installationLocation, javax.swing.GroupLayout.DEFAULT_SIZE, 455, Short.MAX_VALUE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(jButton1))
+                        .addComponent(browseButton))
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jLabel1)
+                        .addComponent(locationLabel)
                         .addGap(0, 0, Short.MAX_VALUE)))
                 .addContainerGap())
         );
@@ -54,19 +73,27 @@ public class LibertyInstanceManagerPanel extends javax.swing.JPanel {
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(jLabel1)
+                .addComponent(locationLabel)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jButton1))
+                    .addComponent(installationLocation, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(browseButton))
                 .addContainerGap(240, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
 
+    private void browseButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_browseButtonActionPerformed
+        // TODO open file browser for Liberty location selection.
+        LibertyInstanceLocationChooser locationChooser = new LibertyInstanceLocationChooser(jFrame1, true);
+        locationChooser.setManagerPanel(this);
+    }//GEN-LAST:event_browseButtonActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton jButton1;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JTextField jTextField1;
+    private javax.swing.JButton browseButton;
+    private javax.swing.JTextField installationLocation;
+    private javax.swing.JFileChooser jFileChooser1;
+    private javax.swing.JFrame jFrame1;
+    private javax.swing.JLabel locationLabel;
     // End of variables declaration//GEN-END:variables
 }
